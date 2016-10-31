a16a4e9d57f0f8d304f83e84a952bec5e7eb387a
srcole <scott.cole0@gmail.com>
2016-10-31 12:46:22 -0700

    add feature saving and loading workflows

--- misc.py
+++ misc.py
@@ -2,28 +2,29 @@
 """
 Miscellaneous useful functions, including:
 
-resample_coupling: calculate significance with resampling
-_z2p: convert z score to p value
-getjetrgb: get the color values to plot in jet colors
-linfit - calculate the linear fit of 2D data
-regressout - regress 1 variable out of another
-norm01 - normalize a series of numbers to have a minimum of 0 and max of 1
-pearsonp - calculate the p-value for a pearson correlation form r and n
-subplot_dims - calculate dimensions for a subplot of a certain number of plots
+1. resample_coupling: calculate significance with resampling
+2. _z2p: convert z score to p value
+3. getjetrgb: get the color values to plot in jet colors
+4. linfit - calculate the linear fit of 2D data
+5. regressout - regress 1 variable out of another
+6. norm01 - normalize a series of numbers to have a minimum of 0 and max of 1
+7. pearsonp - calculate the p-value for a pearson correlation form r and n
+8. subplot_dims - calculate dimensions for a subplot of a certain number of plots
 
-added some new functions that need to be formatted especially:
-pdf2text - extract text from a pdf
-emailme - email me when a script is done
-add2path - add a directory to the file path
-addenvvar - add an environmental variable
-
-run_pdb_on_break - run python debugger when the input function breaks
+9. pdf2text - extract text from a pdf
+10. emailme - email me when a script is done
+11. add2path - add a directory to the file path
+12. addenvvar - add an environmental variable
+13. run_pdb_on_break - run python debugger when the input function breaks
+14. save_features_general - save features as done for seizure data
 """
 
 from __future__ import division
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import glob
+import pandas as pd
 
 def resample_coupling(x1, x2, couplingfn,
                       cfn_dict = {}, Nshuff=100, min_change=.1):
@@ -165,6 +166,7 @@ def subplot_dims(N):
     
 def run_pdb_on_break(fun, args):
     """Run a function and open up pdb when it breaks
+    * If only 1 arg, must be entered in a list, i.e. [args]
     """
     import pdb, traceback, sys
     try:
@@ -173,3 +175,76 @@ def run_pdb_on_break(fun, args):
         type, value, tb = sys.exc_info()
         traceback.print_exc()
         pdb.post_mortem(tb)
+        
+        
+def save_features_general(feature_fn, feature_kwargs,
+                          load_data_fn, load_data_kwargs,
+                          load_bulk_data = True,
+                          load_filenames_fn=None, load_filenames_kwargs=None,
+                          output_folder = 'C:/gh/data2/temp', output_filename_append='',
+                          print_progress = False):
+    """
+    Function to save a feature of many signals to a dataframe
+    
+    Inputs
+    ------
+    
+    Outputs
+    -------
+    
+    Notes
+    -----
+    * If load_data_fn returns None, then use the previous filename
+    """
+    
+    # Load filenames or data
+    if load_bulk_data:
+        data_all = load_data_fn(**load_data_kwargs)
+        N = len(data_all)
+    else:
+        filenames = load_filenames_fn(**load_filenames_kwargs)
+        N = len(filenames)
+        
+    # Calculate features
+    features_dict = np.zeros(N,dtype=dict)
+    for n in range(N):
+        if load_bulk_data:
+            features_dict[n] = feature_fn(data_all[n],**feature_kwargs)
+        else:
+            # Load data
+            data_temp = load_data_fn(filenames[n], **load_data_kwargs)
+            # Deal with case of missing data
+            n_use = n - 1
+            while data_temp is None:
+                data_temp = load_data_fn(filenames[n_use], **load_data_kwargs)
+                n_use -= 1
+            # Calculate features
+            features_dict[n] = feature_fn(data_temp, **feature_kwargs)
+        if print_progress:
+            print 'Calculating features for file '+str(n+1)+'/'+str(N)
+        
+    # Combine dicts by key
+    features_dict_all = {}
+    for k in features_dict[0].iterkeys():
+        features_dict_all[k] = tuple(d[k] for d in features_dict)
+    
+    # Convert dicts to df
+    df = pd.DataFrame.from_dict(features_dict_all)
+    
+    # Derive output filename from feature_fn
+    output_filename = output_folder + feature_fn.func_name + output_filename_append + '.csv'
+    
+    # Save df to csv
+    df.to_csv(output_filename)
+    
+    
+def load_features_general(features_filepath):
+    """Load a set of features from csvs into dataframes"""
+    # Get filenames for all features
+    filenames = glob.glob(features_filepath+'*.csv')
+    # Load all csvs into one data frame
+    dfs = [0]*len(filenames)
+    for n, fi in enumerate(filenames):
+        dfs[n] = pd.read_csv(fi,index_col=0)
+    df = pd.concat(dfs,axis=1)
+    return df
